8a26c9ec4e086a5bf16b49d845a953bb28bce886
caloja1014 <caloja@espol.edu.ec>
2023-04-23 21:20:08 -0500

    adding services

---
+++ lib/api/poke_api.dart
@@ -0,0 +1,31 @@
+import 'package:flisol_taller_flutter/model/pokemon.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PokeApi {
+  static const String baseUrl = 'https://pokeapi.co/api/v2/';
+  static Future<Pokemon> getPokemon(String name) async {
+    final response = await http.get(Uri.parse('$baseUrl/pokemon/$name'));
+    final pokemon = Pokemon.fromJson(jsonDecode(response.body));
+    if (response.statusCode == 200) {
+      return pokemon;
+    } else {
+      throw Exception('Failed to load pokemon');
+    }
+  }
+
+  static Future getPokemons({
+    int limit = 20,
+    int offset = 0,
+  }) async {
+    final response = await http
+        .get(Uri.parse('$baseUrl/pokemon?limit=$limit&offset=$offset'));
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      final pokemons = json['results'];
+      return pokemons;
+    } else {
+      throw Exception('Failed to load pokemons');
+    }
+  }
+}
